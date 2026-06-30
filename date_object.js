30c10f3419ff08ae04259f63d05270e488cef341
NagaLokesh Battula <nagalokesh@paithacs.com>
2026-06-30 16:50:29 +0530

    Implement date_object.js to display date details
    Log various date and time components using Date object methods.

---
+++ date_object.js
@@ -0,0 +1,15 @@
+ let abc=new Date()
+        console.log(abc.getFullYear());              //-----> 2026
+        console.log(abc.getDate());                 //-----> 30
+        console.log(abc.getDay());                 //-----> 2
+        console.log(abc.getMonth());              //-----> 5
+        console.log(abc.toLocaleString());       //----> present date and time
+        console.log(abc.getHours());            //-----> present time in 24h
+        console.log(abc.getMilliseconds());    //-----> present milliseconds
+        console.log(abc.getMinutes());        //-----> present minutes
+        console.log(abc.getSeconds());       //----->  present seconds
+        console.log(abc.getTime());         //----->  present time 
+        console.log(abc.getUTCDate());     //-----> 30
+        console.log(abc.getUTCDay());     //----->  2
+        
+        
